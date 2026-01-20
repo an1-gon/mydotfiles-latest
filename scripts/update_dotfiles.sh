5dae57d9a919980e18170ff14e24440c27fbd056
an1-gon <kurokaidenx@gmail.com>
2026-01-20 11:17:05 -0400

    dot files updated on 20-January-2026

--- scripts/update_dotfiles.sh
+++ scripts/update_dotfiles.sh
@@ -8,6 +8,6 @@ git add . #> /dev/null 2&>1
 
 git commit -m "dot files updated on $(date +%d-%B-%Y)" #> /dev/null 2&>1
 
-git push -u origin main > /dev/null 2&>1
+git push -u origin main #> /dev/null 2&>1
 
 echo "Dot files updated to github bitch am a boss"
